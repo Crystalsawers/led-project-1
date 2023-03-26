a85e244a117af824241639b0af4aefc26a565582
Crystal Sawers <sawece1@student.op.ac.nz>
2023-03-27 10:02:05 +1300

    Add needed libraries

--- countdown_timer/countdown_timer.ino
+++ countdown_timer/countdown_timer.ino
@@ -1,9 +1,21 @@
+#include <SevSeg.h>
+#include <IRremote.hpp>
+
+SevSeg display;
+
 void setup() {
   // put your setup code here, to run once:
-
+  byte numDigits = 4;
+  byte digitPins[] = { 6, 8, 9, 12 };
+  byte segmentPins[] = { 1, 2, 3, 4, 5, 7, 10, 11 };
+  bool resistorsOnSegments = true;
+  bool updateWithDelaysIn = true;
+  byte hardwareConfig = COMMON_CATHODE;
+  display.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  display.setBrightness(100);
+  // Reference: https://electronics-project-hub.com/interfacing-7-segment-display-with-arduino/ 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
