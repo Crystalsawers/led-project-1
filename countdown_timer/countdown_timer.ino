7f82ab71306c3cbe93baeb069ea7909c171e39bc
Crystal Sawers <sawece1@student.op.ac.nz>
2023-04-03 09:36:03 +1200

    play with minutes in timer

--- countdown_timer/countdown_timer.ino
+++ countdown_timer/countdown_timer.ino
@@ -3,11 +3,14 @@
 
 SevSeg display;
 
+int startTimer = 10;
+int endTimer = 0;
+
 void setup() {
   // Setting up the 4-digit, 7-segment display
   byte numDigits = 4;
   byte digitPins[] = { 12, 9, 8, 6 };
-  byte segmentPins[] = { 11, 7, 4, 2, 1, 10, 5, 3 }; // based on the segment pins from CC56-12SURKWA datasheet, A, B, C, D, E, F, G, DP
+  byte segmentPins[] = { 11, 7, 4, 2, 1, 10, 5, 3 };  // based on the segment pins from CC56-12SURKWA datasheet, A, B, C, D, E, F, G, DP
   bool resistorsOnSegments = true;
   bool updateWithDelaysIn = true;
   byte hardwareConfig = COMMON_CATHODE;
@@ -26,7 +29,28 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-// have different numbers counting down in a loop from 1 minute
-  display.setNumber(0, 4);
+  countdown();
+  remote();
+  playATune();
+}
+
+/* Function where the 1 minute timer in 7 segment display counts down */
+void countdown() {
+
+  // have different numbers counting down in a loop from 1 minute
+  
+  display.setNumber(startTimer,4);
   display.refreshDisplay();
+}
+
+/* Function to use IR remote sensor*/
+void remote() {
+
+
+}
+
+/* Function where the buzzer plays after the countdown timer is finished */
+void playATune() {
+
+
 }
