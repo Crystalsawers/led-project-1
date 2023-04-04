60c997819c5b07098f3332da67b0d4af40e99450
Crystalsawers <sawece1@student.op.ac.nz>
2023-04-04 12:43:04 +1200

    change config for digit pins and set timer to 60 sec

--- countdown_timer/countdown_timer.ino
+++ countdown_timer/countdown_timer.ino
@@ -3,21 +3,25 @@
 
 SevSeg display;
 
-static unsigned long timer = millis();
-int seconds = 12;
+unsigned long timer = millis();
+int seconds = 61; // this is including zero
 
 void setup() {
   // Setting up the 4-digit, 7-segment display
   byte numDigits = 4;
   byte digitPins[] = { 12, 9, 8, 6 };
   byte segmentPins[] = { 11, 7, 4, 2, 1, 10, 5, 3 };  // based on the segment pins from CC56-12SURKWA datasheet, A, B, C, D, E, F, G, DP
-  bool resistorsOnSegments = true;
+  // got rid of segment pin 3 as it is a decimal only  due to running out of digital pins/outputs 
+  bool resistorsOnSegments = false;
   bool updateWithDelaysIn = true;
   bool leadingZeros = true;
+  bool disableDecPoint = true; // Use 'true' if your decimal point doesn't exist or isn't connected
   byte hardwareConfig = COMMON_CATHODE;
 
-  display.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
-  display.setBrightness(100);
+
+  display.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments, disableDecPoint);
+  // display.setBrightness(-200); - display brightness ranges in the negatives from -200 to 0
+    display.setBrightness(70);
 }
 
 void loop() {
