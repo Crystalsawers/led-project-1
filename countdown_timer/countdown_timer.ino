06c9eef7246312dcc5e254cccf6d56c87cdd311b
Crystalsawers <sawece1@student.op.ac.nz>
2023-04-28 17:46:16 +1200

    add diagram photos and updated code

--- countdown_timer/countdown_timer.ino
+++ countdown_timer/countdown_timer.ino
@@ -1,63 +1,84 @@
 #include <SevSeg.h>
-#include <IRremote.hpp>
-
 SevSeg display;
 
 unsigned long timer = millis();
-int seconds = 21;  // this is including zero
+int seconds = 31;  // this is including zero
 
 int soundPin = 3;
 
-
 void setup() {
-  // Setting up the 4-digit, 7-segment display
+  /* Setting up the 4-digit, 7-segment display:
+  
+    digitPins array is ordered based on the CC56-12SURKWA datasheet:
+
+    Digit 1: Pin 12,
+    Digit 2: Pin 9,
+    Digit 3: Pin 8,
+    Digit 4: Pin 6
+
+    Segment pins order
+
+    A, B, C, D, E, F, G, DP
+    11, 7, 4, 2, 1, 10, 5      
+ 
+    Note: I got rid of segment pin 3 as it is a decimal only, due to running out of digital pins/outputs
+        
+    In my design I have put resistors on the digit pins in order to use less jumper wires on this display
+
+ */
+
   byte numDigits = 4;
-  byte digitPins[] = { 12, 9, 8, 6 };
-  byte segmentPins[] = { 11, 7, 4, 2, 1, 10, 5 };  // based on the segment pins from CC56-12SURKWA datasheet, A, B, C, D, E, F, G, DP
-  // got rid of segment pin 3 as it is a decimal only  due to running out of digital pins/outputs
+  byte digitPins[] = { 12, 9, 8, 6 }; 
+  byte segmentPins[] = { 11, 7, 4, 2, 1, 10, 5 };  // based on the order of segment pins
   bool resistorsOnSegments = false;
   bool updateWithDelaysIn = true;
   bool leadingZeros = true;
-  bool disableDecPoint = true;  // Use 'true' if your decimal point doesn't exist or isn't connected
+  bool disableDecPoint = true;  // As I ommitted pin 3 the decimal point I set this to true
   byte hardwareConfig = COMMON_CATHODE;
 
   display.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments, disableDecPoint);
   // display.setBrightness(-200); - display brightness ranges in the negatives from -200 to 0
   display.setBrightness(70);
 
-  // buzzer
+  /* Buzzer */
   pinMode(soundPin, OUTPUT);
 
-  //LED
+  /* LEDs */
   pinMode(A0, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  //blink();
+  // I used the countdown functionality into a seperate function here, because I didn't want to put everything in loop at once
   countdown();
-
-  // remote();
 }
 
 /* Function where the 1 minute timer in 7 segment display counts down */
 
 void countdown() {
 
-
-  // Wait for a short period of time before starting the countdown again
-
   if (millis() - timer >= 1000) {
 
+    digitalWrite(13, HIGH); // turn on Yellow LED to signal that the timer is in progress
     timer += 1000;
     seconds--;
+
     tone(soundPin, 247, 100);
 
-    if (seconds < 0) {
+    if (seconds < 10) {
+
+      digitalWrite(13, HIGH);    
+      digitalWrite(A0, HIGH); // turn on Blue LED to signal that time is nearly up
+      delay(1000); // gives flashing effect on both clock and led
+      digitalWrite(A0, LOW);
+
+      
+    }
+    if (seconds <= 0) {
       seconds = 0;
-      digitalWrite(A0, HIGH);
-      playATune();  // call the function to play a song
+      digitalWrite(13, LOW);
+      digitalWrite(A0, HIGH);  // LED flashes when finished
+      playATune();           // call the function to play a song
     }
 
     display.setNumber(seconds, 3);
@@ -69,59 +90,19 @@ void countdown() {
 /* Function where the buzzer plays after the countdown timer is finished */
 void playATune() {
 
+  // Song played: nokia 3310 ringtone
 
-// nokia 3310 ringtone 
+  int numNotes = 13;
 
-  buzzerSounds('C', 1);
-  buzzerSounds('E', 4);
-  buzzerSounds('G', 2);
+  int frequencies[] = { 1318, 1174, 739, 830, 1109, 988, 587, 659, 988, 880, 554, 659, 880 };
 
-  buzzerSounds(' ', 2);
+  int rhythm[] = { 2, 2, 4, 4, 2, 2, 4, 4, 2, 2, 4, 4, 8 };
 
-  buzzerSounds('C', 1);
-  buzzerSounds('E', 4);
-  buzzerSounds('G', 2);
-}
-
-// this function is for all the notes in a song
-void buzzerSounds(char note, int beats) {
-
-  int numNotes = 14;  // number of notes in our note and frequency array (there are 15 values, but arrays start at 0)
-
-  //Note: these notes are C major (there are no sharps or flats)
-
-  //this array is used to look up the notes
-  char notes[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C', 'D', 'E', 'F', 'G', 'A', 'B', ' ' };
-  //this array matches frequencies with each letter (e.g. the 4th note is 'f', the 4th frequency is 175)
-  int frequencies[] = { 131, 147, 165, 175, 196, 220, 247, 262, 294, 330, 349, 392, 440, 494, 0 };
-
-  int currentFrequency = 0;  //the frequency that we find when we look up a frequency in the arrays
-  int beatLength = 150;      //the length of one beat (changing this will speed up or slow down the tempo of the song)
-
-  //look up the frequency that corresponds to the note
-  for (int i = 0; i < numNotes; i++)  // check each value in notes from 0 to 14
-  {
-    if (notes[i] == note)  // does the letter passed to the play function match the letter in the array?
-    {
-      currentFrequency = frequencies[i];  // Yes! Set the current frequency to match that note
-    }
+  for (int i = 0; i < numNotes; i++) {
+    tone(soundPin, frequencies[i], 150 * rhythm[i]);
+    delay(60 * rhythm[i]);
   }
-
-  //play the frequency that matched our letter for the number of beats passed to the play function
-  tone(soundPin, currentFrequency, beats * beatLength);
-  delay(beats * beatLength);  //wait for the length of the tone so that it has time to play
-  delay(50);                  //a little delay between the notes makes the song sound more natural
-}
-
-void remote() {
-
-  // IR remote stuff
-}
-
-void blink() {
-
-    digitalWrite(A0, HIGH);
-    delay(100);
-    digitalWrite(A0, LOW);
-    delay(1000);
+  noTone(soundPin);
+  digitalWrite(A0, LOW);  
+  delay(5000);
 }
